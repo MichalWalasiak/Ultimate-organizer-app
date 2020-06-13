a6f06e149f8dc25b7d852ec50718be109fd7ea39
MichalWalasiak <mwalasiak@gmail.com>
2020-06-13 18:55:28 +0200

    Add constructor to GroupReadModel class

--- src/main/java/io/github/w7mike/model/projection/GroupReadModel.java
+++ src/main/java/io/github/w7mike/model/projection/GroupReadModel.java
@@ -1,9 +1,12 @@
 package io.github.w7mike.model.projection;
 
+import io.github.w7mike.model.Job;
 import io.github.w7mike.model.JobGroups;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class GroupReadModel {
 
@@ -17,12 +20,18 @@ public class GroupReadModel {
 
     public GroupReadModel(JobGroups source){
         specification = source.getSpecification();
+            source.getJobs().stream()
+                    .sorted(Comparator.comparing(Job::getDeadline))
+                    .map(Job::getDeadline)
+                    .max(LocalDateTime::compareTo)
+                    .ifPresent(date -> deadline = date);
 
-//        source.getJ.stream()
-//                .
+            jobs = source.getJobs()
+                    .stream()
+                    .map(GroupJobReadModel::new)
+                    .collect(Collectors.toSet());
+
     }
-
-
 
     public String getSpecification() {
         return specification;
