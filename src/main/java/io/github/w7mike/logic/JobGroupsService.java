7f7dc371736162975ef0eb0daceecb70b24253a8
MichalWalasiak <mwalasiak@gmail.com>
2020-06-15 19:09:10 +0200

    Add methods createGroup and readAll to JobGroupsService class.

--- src/main/java/io/github/w7mike/logic/JobGroupsService.java
+++ src/main/java/io/github/w7mike/logic/JobGroupsService.java
@@ -1,8 +1,14 @@
 package io.github.w7mike.logic;
 
 
+import io.github.w7mike.model.JobGroups;
 import io.github.w7mike.model.JobGroupsRepository;
+import io.github.w7mike.model.projection.GroupReadModel;
+import io.github.w7mike.model.projection.GroupWriteModel;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class JobGroupsService {
@@ -12,4 +18,18 @@ public class JobGroupsService {
     public JobGroupsService(final JobGroupsRepository repository) {
         this.repository = repository;
     }
+
+    public GroupReadModel createGroup(GroupWriteModel source){
+        JobGroups result = repository.save(source.toGroup());
+        return new GroupReadModel(result);
+    }
+
+    public Set<GroupReadModel> readAll(){
+        return repository.findAll()
+                .stream()
+                .map(GroupReadModel::new)
+                .collect(Collectors.toSet());
+    }
+
+
 }
