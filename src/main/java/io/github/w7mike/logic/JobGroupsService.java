fe88aca4a725082551b267ff8a5e402e00acf945
MichalWalasiak <mwalasiak@gmail.com>
2020-06-15 19:03:26 +0200

    Create private field of JobGroupsRepository and initialize it with public constructor.

--- src/main/java/io/github/w7mike/logic/JobGroupsService.java
+++ src/main/java/io/github/w7mike/logic/JobGroupsService.java
@@ -1,11 +1,15 @@
 package io.github.w7mike.logic;
 
 
+import io.github.w7mike.model.JobGroupsRepository;
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class JobGroupsService {
 
+    private JobGroupsRepository repository;
 
+    public JobGroupsService(final JobGroupsRepository repository) {
+        this.repository = repository;
+    }
 }
