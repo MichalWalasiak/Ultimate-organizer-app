d0ebd62cd4c3c886fc4d2f5e1a3002bceb50436d
MichalWalasiak <mwalasiak@gmail.com>
2020-06-16 18:41:23 +0200

    Create private fields: JobRepository, JobGroupRepository, JobConfigurationProperties. Initialize them in public constructor of ProjectsService class.

--- src/main/java/io/github/w7mike/logic/ProjectsService.java
+++ src/main/java/io/github/w7mike/logic/ProjectsService.java
@@ -1,7 +1,20 @@
 package io.github.w7mike.logic;
 
+import io.github.w7mike.JobConfigurationProperties;
+import io.github.w7mike.model.JobGroupsRepository;
+import io.github.w7mike.model.JobRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProjectsService {
+
+    private JobRepository repository;
+    private JobGroupsRepository groupsRepository;
+    private JobConfigurationProperties properties;
+
+    public ProjectsService(final JobRepository repository, final JobGroupsRepository groupsRepository, final JobConfigurationProperties properties) {
+        this.repository = repository;
+        this.groupsRepository = groupsRepository;
+        this.properties = properties;
+    }
 }
