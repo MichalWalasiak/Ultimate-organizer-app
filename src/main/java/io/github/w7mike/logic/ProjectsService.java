ace91f6e6d976dabd5efd04a3874b06f00d87dc1
MichalWalasiak <mwalasiak@gmail.com>
2020-06-16 18:48:19 +0200

    Change JobRepository field into ProjectRepository type.

--- src/main/java/io/github/w7mike/logic/ProjectsService.java
+++ src/main/java/io/github/w7mike/logic/ProjectsService.java
@@ -3,18 +3,24 @@ package io.github.w7mike.logic;
 import io.github.w7mike.JobConfigurationProperties;
 import io.github.w7mike.model.JobGroupsRepository;
 import io.github.w7mike.model.JobRepository;
+import io.github.w7mike.model.ProjectRepository;
+import io.github.w7mike.model.Projects;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProjectsService {
 
-    private JobRepository repository;
+    private ProjectRepository repository;
     private JobGroupsRepository groupsRepository;
     private JobConfigurationProperties properties;
 
-    public ProjectsService(final JobRepository repository, final JobGroupsRepository groupsRepository, final JobConfigurationProperties properties) {
+    public ProjectsService(final ProjectRepository repository, final JobGroupsRepository groupsRepository, final JobConfigurationProperties properties) {
         this.repository = repository;
         this.groupsRepository = groupsRepository;
         this.properties = properties;
     }
+
+
 }
