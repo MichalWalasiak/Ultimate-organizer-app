0104d4d7c2a9f943f7deff1eafa465f11254c374
MichalWalasiak <mwalasiak@gmail.com>
2020-06-17 10:52:45 +0200

    Create ProjectsServiceTest class

---
+++ src/test/java/io/github/w7mike/logic/ProjectsServiceTest.java
@@ -0,0 +1,12 @@
+package io.github.w7mike.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectsServiceTest {
+
+    @Test
+    void createGroup() {
+    }
+}
