f23a52344c1dbc0458af72ce14f88ee991764290
MichalWalasiak <mwalasiak@gmail.com>
2020-06-17 17:55:42 +0200

    Create mock objects and assertion in createGroups_NoMultipleGroupsProperties_And_IncompleteGroupsExists_throwsIllegalStateException method to test throwing exception.

--- src/test/java/io/github/w7mike/logic/ProjectsServiceTest.java
+++ src/test/java/io/github/w7mike/logic/ProjectsServiceTest.java
@@ -1,14 +1,42 @@
 package io.github.w7mike.logic;
 
+import io.github.w7mike.JobConfigurationProperties;
+import io.github.w7mike.model.JobGroupsRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 
 class ProjectsServiceTest {
 
     @Test
     @DisplayName("Should throw IllegalStateException when properties allows one groups and other incomplete groups exist")
     void createGroup_NoMultipleGroupsProperties_And_IncompleteGroupsExists_throwsIllegalStateException() {
+        //given
+        var mockGroupRepository = mock(JobGroupsRepository.class);
+        when(mockGroupRepository.existsByCompleteIsFalseAndProjects_Id(anyInt())).thenReturn(true);
+        //and
+        var mockTemplate = mock(JobConfigurationProperties.Template.class);
+        when(mockTemplate.isAllowMultipleJobs()).thenReturn(false);
+        //and
+        var mockProperties = mock(JobConfigurationProperties.class);
+        when(mockProperties.getTemplate()).thenReturn(mockTemplate);
+        //System Under Test
+        var toTest = new ProjectsService(null, mockGroupRepository, mockProperties);
+
+        //when
+        var exception = catchThrowable(()-> toTest.createGroup(LocalDateTime.now(), 0));
+
+        //then
+
+        assertThat(exception)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("one incomplete group");
+
     }
 }
