ba0750d2b18db09416463cc1cbe6ae761ed2a8c0
MichalWalasiak <mwalasiak@gmail.com>
2020-06-17 19:02:24 +0200

    Add test method to test throwing IllegalArgumentException when first condition is not fulfilleld .

--- src/test/java/io/github/w7mike/logic/ProjectsServiceTest.java
+++ src/test/java/io/github/w7mike/logic/ProjectsServiceTest.java
@@ -39,4 +39,30 @@ class ProjectsServiceTest {
                 .hasMessageContaining("one incomplete group");
 
     }
+
+    @Test
+    @DisplayName("Should throw IllegalArgumentException when configuration ok and no projects exists with given id")
+    void createGroup_ConfigurationOk_And_noProjects_throwsIllegalArgumentException() {
+        //given
+        var mockGroupRepository = mock(JobGroupsRepository.class);
+        when(mockGroupRepository.existsByCompleteIsFalseAndProjects_Id(anyInt())).thenReturn(true);
+        //and
+        var mockTemplate = mock(JobConfigurationProperties.Template.class);
+        when(mockTemplate.isAllowMultipleJobs()).thenReturn(false);
+        //and
+        var mockProperties = mock(JobConfigurationProperties.class);
+        when(mockProperties.getTemplate()).thenReturn(mockTemplate);
+        //System Under Test
+        var toTest = new ProjectsService(null, mockGroupRepository, mockProperties);
+
+        //when
+        var exception = catchThrowable(()-> toTest.createGroup(LocalDateTime.now(), 0));
+
+        //then
+
+        assertThat(exception)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("one incomplete group");
+
+    }
 }
