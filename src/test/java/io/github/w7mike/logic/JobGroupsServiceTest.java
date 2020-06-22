805542f0f74bfa3d2aa17da325645aeea2c0b6ec
Michał Walasiak <m.walasiak@gmail.com>
2020-06-22 20:36:20 +0200

    Fill given and when sections in test method

--- src/test/java/io/github/w7mike/logic/JobGroupsServiceTest.java
+++ src/test/java/io/github/w7mike/logic/JobGroupsServiceTest.java
@@ -1,15 +1,29 @@
 package io.github.w7mike.logic;
 
+import io.github.w7mike.model.JobRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class JobGroupsServiceTest {
 
     @Test
     @DisplayName("Should Throw IllegalStateException when incomplete jobs exists")
     void toggleGroup_incompleteGroups_throwsIllegalStateException(){
+        //given
+        var mockJobRepository = mock(JobRepository.class);
+        when(mockJobRepository.existsByCompleteIsFalseAndJobGroups_Id(anyInt())).thenReturn(true);
+
+        var toTest = new JobGroupsService(null, mockJobRepository);
+        //when
+
+        var exception = catchThrowable(()-> toTest.toggleGroup(1));
+        //then
 
     }
 
