10668fdfad1e7c2e7ebd1fcb7fa9ef8f822e4242
Michał Walasiak <m.walasiak@gmail.com>
2020-06-22 19:12:31 +0200

    Create first test method

--- src/test/java/io/github/w7mike/logic/JobGroupsServiceTest.java
+++ src/test/java/io/github/w7mike/logic/JobGroupsServiceTest.java
@@ -1,7 +1,16 @@
 package io.github.w7mike.logic;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class JobGroupsServiceTest {
+
+    @Test
+    @DisplayName("Should Throw IllegalStateException when incomplete jobs exists")
+    void toggleGroup_incompleteGroups_throwsIllegalStateException(){
+
+    }
 
 }
