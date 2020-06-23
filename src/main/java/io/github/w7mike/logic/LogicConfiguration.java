48f887777fff18d0aa5c7a3bae883bffc93b473c
Michał Walasiak <m.walasiak@gmail.com>
2020-06-23 13:37:28 +0200

    Add Configuration and Bean annotations

--- src/main/java/io/github/w7mike/logic/LogicConfiguration.java
+++ src/main/java/io/github/w7mike/logic/LogicConfiguration.java
@@ -1,4 +1,13 @@
 package io.github.w7mike.logic;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class LogicConfiguration {
+
+    @Bean
+    public ProjectsService service(){
+
+    }
 }
