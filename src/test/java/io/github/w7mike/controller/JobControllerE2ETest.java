2784a822e381d2848dcce20d93391010f713c161
Michał Walasiak <m.walasiak@gmail.com>
2020-06-24 16:50:24 +0200

    Add data to class

--- src/test/java/io/github/w7mike/controller/JobControllerE2ETest.java
+++ src/test/java/io/github/w7mike/controller/JobControllerE2ETest.java
@@ -1,10 +1,24 @@
 package io.github.w7mike.controller;
 
+import io.github.w7mike.model.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class JobControllerE2ETest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    JobRepository jobRepository;
+
 
 }
