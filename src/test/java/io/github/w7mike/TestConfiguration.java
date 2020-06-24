670288a0c6a1c1855b7aae36ab5ac3654f4ef255
Michał Walasiak <m.walasiak@gmail.com>
2020-06-24 17:23:44 +0200

    Add Primary annotation

--- src/test/java/io/github/w7mike/TestConfiguration.java
+++ src/test/java/io/github/w7mike/TestConfiguration.java
@@ -4,6 +4,7 @@ import io.github.w7mike.model.Job;
 import io.github.w7mike.model.JobRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +15,7 @@ import java.util.*;
 class TestConfiguration {
 
     @Bean
+    @Primary
     @Profile("integration")
     JobRepository testRepository(){
         return new JobRepository() {
