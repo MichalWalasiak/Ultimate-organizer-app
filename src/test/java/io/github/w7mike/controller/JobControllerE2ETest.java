df5d1dbdc5319acefb1fc10b977d6df4f1a5f2b9
Michał Walasiak <m.walasiak@gmail.com>
2020-06-24 17:03:25 +0200

    Fill given and when sections

--- src/test/java/io/github/w7mike/controller/JobControllerE2ETest.java
+++ src/test/java/io/github/w7mike/controller/JobControllerE2ETest.java
@@ -1,10 +1,14 @@
 package io.github.w7mike.controller;
 
+import io.github.w7mike.model.Job;
 import io.github.w7mike.model.JobRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +23,17 @@ class JobControllerE2ETest {
 
     @Autowired
     JobRepository jobRepository;
+
+    @Test
+    void httpGet_returnAllJobs(){
+        // given
+        jobRepository.save(new Job("foo", LocalDateTime.now()));
+        jobRepository.save(new Job("bar", LocalDateTime.now()));
+
+        // when
+        Job[] outcome = restTemplate.getForObject("http://localhost:" + port + "/jobs", Job[].class);
+        // then
+    }
 
 
 }
