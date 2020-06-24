cc0711b3f9a4b0116c15173137ae3089e73f41d1
Michał Walasiak <m.walasiak@gmail.com>
2020-06-24 16:46:54 +0200

    Add SpringBootTest annotation

--- src/test/java/io/github/w7mike/controller/JobControllerE2ETest.java
+++ src/test/java/io/github/w7mike/controller/JobControllerE2ETest.java
@@ -1,7 +1,10 @@
 package io.github.w7mike.controller;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class JobControllerE2ETest {
 
 }
