015999edb0100d53e245c6ffd977d6d41f238c43
Michał Walasiak <m.walasiak@gmail.com>
2020-06-24 13:46:38 +0200

    Create bean for JobGroupService

--- src/main/java/io/github/w7mike/logic/LogicConfiguration.java
+++ src/main/java/io/github/w7mike/logic/LogicConfiguration.java
@@ -2,6 +2,7 @@ package io.github.w7mike.logic;
 
 import io.github.w7mike.JobConfigurationProperties;
 import io.github.w7mike.model.JobGroupsRepository;
+import io.github.w7mike.model.JobRepository;
 import io.github.w7mike.model.ProjectRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +17,13 @@ public class LogicConfiguration {
             final JobConfigurationProperties properties
     ){
         return new ProjectsService(projectRepository, groupsRepository, properties);
+    }
+
+    @Bean
+    public JobGroupsService jobGroupsService(
+            final JobGroupsRepository groupsRepository,
+            final JobRepository jobRepository
+    ){
+        return new JobGroupsService(groupsRepository, jobRepository);
     }
 }
