485f6097ad0a364947ed5e4ac0e0641f46ea3e8a
Michał Walasiak <m.walasiak@gmail.com>
2020-06-24 12:51:49 +0200

    Add @Profile annotation
    This is to resolve bean conflict.

--- src/test/java/io/github/w7mike/TestConfiguration.java
+++ src/test/java/io/github/w7mike/TestConfiguration.java
@@ -4,6 +4,7 @@ import io.github.w7mike.model.Job;
 import io.github.w7mike.model.JobRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -13,6 +14,7 @@ import java.util.*;
 class TestConfiguration {
 
     @Bean
+    @Profile("integration")
     JobRepository testRepository(){
         return new JobRepository() {
             private Map<Integer, Job> jobs = new HashMap<>();
