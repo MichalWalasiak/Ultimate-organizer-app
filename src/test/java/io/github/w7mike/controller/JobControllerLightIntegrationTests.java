0d5b1abf05aa00f1296dae69611b7b8a8c8a2740
Michał Walasiak <m.walasiak@gmail.com>
2020-06-25 14:39:07 +0200

    Change JobRepository annotation

--- src/test/java/io/github/w7mike/controller/JobControllerLightIntegrationTests.java
+++ src/test/java/io/github/w7mike/controller/JobControllerLightIntegrationTests.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,7 +22,7 @@ public class JobControllerLightIntegrationTests {
 
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
+    @MockBean
     private JobRepository jobRepository;
 
     @Test
