0c924f581e75b97dbed793085c2ddec5de1e11aa
Michał Walasiak <m.walasiak@gmail.com>
2020-06-25 13:29:20 +0200

    Fill given section

--- src/test/java/io/github/w7mike/controller/JobControllerIntegrationTests.java
+++ src/test/java/io/github/w7mike/controller/JobControllerIntegrationTests.java
@@ -1,5 +1,6 @@
 package io.github.w7mike.controller;
 
+import io.github.w7mike.model.Job;
 import io.github.w7mike.model.JobRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,6 +23,9 @@ public class JobControllerIntegrationTests {
     @Test
     @DisplayName("should return given Job")
     void httpGet_returnsGivenJob() {
+        // given
+        jobRepository.save(new Job("foo", LocalDateTime.now()));
+
 
     }
 }
