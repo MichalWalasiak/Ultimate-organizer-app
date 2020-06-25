69c1cb361714bc984b073515d6e22222a2090ea4
Michał Walasiak <m.walasiak@gmail.com>
2020-06-25 13:15:58 +0200

    Create test class

---
+++ src/test/java/io/github/w7mike/controller/JobControllerIntegrationTests.java
@@ -0,0 +1,9 @@
+package io.github.w7mike.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JobControllerIntegrationTests {
+}
