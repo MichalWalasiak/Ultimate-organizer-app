df7da4826b4894c8bfe18d80ebebb79123912a74
Michał Walasiak <m.walasiak@gmail.com>
2020-06-25 13:50:21 +0200

    Remove unused imports

--- src/test/java/io/github/w7mike/controller/JobControllerIntegrationTests.java
+++ src/test/java/io/github/w7mike/controller/JobControllerIntegrationTests.java
@@ -9,8 +9,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDateTime;
 
