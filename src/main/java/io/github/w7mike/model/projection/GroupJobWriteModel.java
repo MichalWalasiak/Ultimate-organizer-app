305d8c6bca0f032ad8bcb1846681819e66323a00
Michał Walasiak <m.walasiak@gmail.com>
2020-06-25 18:11:38 +0200

    Add parameter to doJob method

--- src/main/java/io/github/w7mike/model/projection/GroupJobWriteModel.java
+++ src/main/java/io/github/w7mike/model/projection/GroupJobWriteModel.java
@@ -1,6 +1,7 @@
 package io.github.w7mike.model.projection;
 
 import io.github.w7mike.model.Job;
+import io.github.w7mike.model.JobGroups;
 
 import java.time.LocalDateTime;
 
@@ -25,7 +26,7 @@ public class GroupJobWriteModel {
         this.deadline = deadline;
     }
 
-    public Job doJob(){
-        return new Job(specification, deadline);
+    public Job doJob(final JobGroups jobGroups){
+        return new Job(specification, deadline, jobGroups);
     }
 }
