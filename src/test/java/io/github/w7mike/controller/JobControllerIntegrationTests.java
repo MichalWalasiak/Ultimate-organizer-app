8caa952f5fed938aa2fc312a22a9dcc64084a423
Michał Walasiak <m.walasiak@gmail.com>
2020-06-25 13:24:47 +0200

    Add private fields to class

--- src/test/java/io/github/w7mike/controller/JobControllerIntegrationTests.java
+++ src/test/java/io/github/w7mike/controller/JobControllerIntegrationTests.java
@@ -1,9 +1,17 @@
 package io.github.w7mike.controller;
 
+import io.github.w7mike.model.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class JobControllerIntegrationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private JobRepository jobRepository;
 }
