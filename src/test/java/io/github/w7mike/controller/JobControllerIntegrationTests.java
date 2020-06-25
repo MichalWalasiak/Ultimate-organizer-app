e51f24c0db607d353deb7d07fd3646cec5ab3b17
Michał Walasiak <m.walasiak@gmail.com>
2020-06-25 13:34:00 +0200

    Fill when + then section

--- src/test/java/io/github/w7mike/controller/JobControllerIntegrationTests.java
+++ src/test/java/io/github/w7mike/controller/JobControllerIntegrationTests.java
@@ -8,8 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,6 +30,12 @@ public class JobControllerIntegrationTests {
     void httpGet_returnsGivenJob() {
         // given
         jobRepository.save(new Job("foo", LocalDateTime.now()));
+
+        // when + then
+        mockMvc.perform(get("/jobs/"))
+                .andExpect(status().is2xxSuccessful());
+
+
 
 
     }
