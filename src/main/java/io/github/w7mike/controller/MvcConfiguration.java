2d7868ab0f52fa8820373149b8c189287584609b
Michał Walasiak <m.walasiak@gmail.com>
2020-06-29 18:28:07 +0200

    Create new class

---
+++ src/main/java/io/github/w7mike/controller/MvcConfiguration.java
@@ -0,0 +1,8 @@
+package io.github.w7mike.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+}
