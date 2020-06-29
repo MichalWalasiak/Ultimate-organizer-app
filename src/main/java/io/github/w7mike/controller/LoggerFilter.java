a36f8a9041a10de06f7008a52c8d7cc001c0a24e
Michał Walasiak <m.walasiak@gmail.com>
2020-06-29 15:05:27 +0200

    Add annotation and implement method

--- src/main/java/io/github/w7mike/controller/LoggerFilter.java
+++ src/main/java/io/github/w7mike/controller/LoggerFilter.java
@@ -1,6 +1,14 @@
 package io.github.w7mike.controller;
 
-import javax.servlet.Filter;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.*;
+import java.io.IOException;
+
+@Component
 public class LoggerFilter implements Filter {
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+
+    }
 }
