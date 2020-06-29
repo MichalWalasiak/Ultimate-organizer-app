fb79d8366e3723ccfddb99cde68732f6649e6d3e
Michał Walasiak <m.walasiak@gmail.com>
2020-06-29 15:13:14 +0200

    Add Logger to class

--- src/main/java/io/github/w7mike/controller/LoggerFilter.java
+++ src/main/java/io/github/w7mike/controller/LoggerFilter.java
@@ -1,5 +1,8 @@
 package io.github.w7mike.controller;
 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -7,6 +10,8 @@ import java.io.IOException;
 
 @Component
 public class LoggerFilter implements Filter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggerFilter.class);
+
     @Override
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
 
