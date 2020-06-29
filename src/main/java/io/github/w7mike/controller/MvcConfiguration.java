28aba2b0e7bb5c09c7200daee9e8de3dda0dbe57
Michał Walasiak <m.walasiak@gmail.com>
2020-06-29 18:35:44 +0200

    Override addInterceptors method

--- src/main/java/io/github/w7mike/controller/MvcConfiguration.java
+++ src/main/java/io/github/w7mike/controller/MvcConfiguration.java
@@ -1,8 +1,13 @@
 package io.github.w7mike.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor());
+    }
 }
