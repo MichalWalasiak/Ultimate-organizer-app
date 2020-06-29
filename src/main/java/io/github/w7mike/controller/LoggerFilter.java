350cb7ef5e5f3cf80698cd8f19d51cd93d1f441c
Michał Walasiak <m.walasiak@gmail.com>
2020-06-29 15:27:36 +0200

    Add condition to method

--- src/main/java/io/github/w7mike/controller/LoggerFilter.java
+++ src/main/java/io/github/w7mike/controller/LoggerFilter.java
@@ -6,14 +6,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Component
 public class LoggerFilter implements Filter {
-    private static final Logger LOGGER = LoggerFactory.getLogger(LoggerFilter.class);
+    private static final Logger logger = LoggerFactory.getLogger(LoggerFilter.class);
 
     @Override
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
-
+        if (request instanceof HttpServletRequest){
+            var httpRequest = (HttpServletRequest) request;
+            logger.info("[doFilter]" + httpRequest.getMethod() + "" + httpRequest.getRequestURI());
+        }
     }
 }
