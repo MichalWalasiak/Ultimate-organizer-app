6df26112960e767408aadba9f60be06277cbcafa
Michał Walasiak <m.walasiak@gmail.com>
2020-06-30 18:38:04 +0200

    Change private field type

--- src/main/java/io/github/w7mike/controller/JobGroupController.java
+++ src/main/java/io/github/w7mike/controller/JobGroupController.java
@@ -3,6 +3,7 @@ package io.github.w7mike.controller;
 import io.github.w7mike.logic.JobGroupService;
 import io.github.w7mike.model.Job;
 import io.github.w7mike.model.JobGroupRepository;
+import io.github.w7mike.model.JobRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -19,35 +20,35 @@ public class JobGroupController {
     private static final Logger logger = LoggerFactory.getLogger(JobGroupController.class);
 
     private JobGroupService jobGroupService;
-    private JobGroupRepository jobGroupRepository;
+    private JobRepository jobRepository;
 
-    public JobGroupController(final JobGroupService jobGroupService, final JobGroupRepository jobGroupRepository) {
+    public JobGroupController(final JobGroupService jobGroupService, final JobRepository jobRepository) {
         this.jobGroupService = jobGroupService;
-        this.jobGroupRepository = jobGroupRepository;
+        this.jobRepository = jobRepository;
     }
 
     @PostMapping
     ResponseEntity<Job> createJob(@RequestBody @Valid Job newJob){
-        Job result = jobGroupRepository.save(newJob);
+        Job result = jobRepository.save(newJob);
         return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
     @GetMapping(params = {"!sort", "!page", "!size"})
     ResponseEntity<List<Job>> readAllJobs(){
         logger.warn("You are going to read all available Jobs");
-        return ResponseEntity.ok(repository.findAll());
+        return ResponseEntity.ok(jobRepository.findAll());
     }
 
     @PutMapping("/{id}")
     ResponseEntity<Job> updateJob(@PathVariable int id, @RequestBody Job jobToUpdate){
-        if(!repository.existsById(id)){
+        if(!jobRepository.existsById(id)){
             return ResponseEntity.notFound().build();
         }
 
-        repository.findById(id)
+        jobRepository.findById(id)
                 .ifPresent(job -> {
                     job.updateFrom(jobToUpdate);
-                    repository.save(job);
+                    jobRepository.save(job);
                 });
         return ResponseEntity.noContent().build();
     }
@@ -55,11 +56,11 @@ public class JobGroupController {
     @Transactional
     @PatchMapping("/{id}")
     public ResponseEntity<Job> toggleJob(@PathVariable int id){
-        if(!repository.existsById(id)){
+        if(!jobRepository.existsById(id)){
             return ResponseEntity.notFound().build();
         }
 
-        Job job = repository.findById(id)
+        Job job = jobRepository.findById(id)
                 .get();
         job.setComplete(!job.isComplete());
 
