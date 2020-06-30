1a67c0b1b5331a42ad65f5ddc15cbf0070b74c66
Michał Walasiak <m.walasiak@gmail.com>
2020-06-30 18:24:46 +0200

    Add fields and constructor to class

--- src/main/java/io/github/w7mike/controller/JobGroupController.java
+++ src/main/java/io/github/w7mike/controller/JobGroupController.java
@@ -1,7 +1,21 @@
 package io.github.w7mike.controller;
 
+import io.github.w7mike.logic.JobGroupService;
+import io.github.w7mike.model.JobGroupRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JobGroupController {
+
+    private static final Logger logger = LoggerFactory.getLogger(JobGroupController.class);
+
+    private JobGroupService jobGroupService;
+    private JobGroupRepository jobGroupRepository;
+
+    public JobGroupController(final JobGroupService jobGroupService, final JobGroupRepository jobGroupRepository) {
+        this.jobGroupService = jobGroupService;
+        this.jobGroupRepository = jobGroupRepository;
+    }
 }
