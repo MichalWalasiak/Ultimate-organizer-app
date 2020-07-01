55e3ef0d8076a6ed60a33d660649d56b73c91d3c
Michał Walasiak <m.walasiak@gmail.com>
2020-07-01 17:15:59 +0200

    Add handler for illegalStateException

--- src/main/java/io/github/w7mike/controller/JobGroupController.java
+++ src/main/java/io/github/w7mike/controller/JobGroupController.java
@@ -55,7 +55,12 @@ public class JobGroupController {
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    ResponseEntity<String> handleIllegalArgument (IllegalArgumentException e) {
+    ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    ResponseEntity<String> handleIllegalState(IllegalStateException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
