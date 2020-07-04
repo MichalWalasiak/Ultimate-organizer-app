09af195f5e4651892b8e583c152790604db9ac53
Michał Walasiak <m.walasiak@gmail.com>
2020-07-04 12:12:47 +0200

    Add method

--- src/main/java/io/github/w7mike/controller/ProjectController.java
+++ src/main/java/io/github/w7mike/controller/ProjectController.java
@@ -1,10 +1,13 @@
 package io.github.w7mike.controller;
 
+import io.github.w7mike.model.ProjectStep;
 import io.github.w7mike.model.projection.GroupWriteModel;
 import io.github.w7mike.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +17,12 @@ class ProjectController {
     @GetMapping
     public String showProjects(Model model) {
         model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+
+    @PostMapping(params = "addStep")
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
+        current.getSteps().add(new ProjectStep());
         return "projects";
     }
 }
