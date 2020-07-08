335bd2c7baa5911be43b9c992092866a8ada508f
Michał Walasiak <m.walasiak@gmail.com>
2020-07-08 14:57:24 +0200

    Add method to Controller class

--- src/main/java/io/github/w7mike/controller/ProjectController.java
+++ src/main/java/io/github/w7mike/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package io.github.w7mike.controller;
 
 import io.github.w7mike.logic.ProjectsService;
+import io.github.w7mike.model.Project;
 import io.github.w7mike.model.ProjectStep;
 import io.github.w7mike.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/projects")
@@ -38,5 +41,10 @@ class ProjectController {
     String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
         current.getSteps().add(new ProjectStep());
         return "projects";
+    }
+
+    @ModelAttribute("projects")
+    List<Project> getProjects() {
+        return service.readAll();
     }
 }
