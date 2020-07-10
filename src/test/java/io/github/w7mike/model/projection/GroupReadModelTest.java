4107009d71c53f6074ebcf36a8e8f5d2bf922cd3
Michał Walasiak <m.walasiak@gmail.com>
2020-07-10 13:39:57 +0200

    Fill given and when section

--- src/test/java/io/github/w7mike/model/projection/GroupReadModelTest.java
+++ src/test/java/io/github/w7mike/model/projection/GroupReadModelTest.java
@@ -1,7 +1,11 @@
 package io.github.w7mike.model.projection;
 
+import io.github.w7mike.model.Job;
+import io.github.w7mike.model.JobGroup;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +14,15 @@ class GroupReadModelTest {
     @Test
     @DisplayName("should create null deadline for jobs where no deadline is given")
     void constructor_noDeadline_createNullDeadline() {
+        // given
+        var source = new JobGroup();
+        source.setSpecification("foo");
+        source.setJobs(Set.of(new Job("bar", null)));
 
+        // when
+        var result = new GroupReadModel(source);
+
+        // then
     }
 
 }
