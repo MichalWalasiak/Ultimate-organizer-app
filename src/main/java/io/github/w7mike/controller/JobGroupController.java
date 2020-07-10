ad94398311b246799850ae17b17edb5a3a8a49c6
Michał Walasiak <m.walasiak@gmail.com>
2020-07-10 14:51:27 +0200

    Add annotations

--- src/main/java/io/github/w7mike/controller/JobGroupController.java
+++ src/main/java/io/github/w7mike/controller/JobGroupController.java
@@ -8,6 +8,7 @@ import io.github.w7mike.model.projection.GroupWriteModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +17,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
+@Controller
 @RequestMapping("/groups")
 public class JobGroupController {
 
@@ -29,24 +30,24 @@ public class JobGroupController {
         this.jobGroupService = jobGroupService;
         this.jobRepository = jobRepository;
     }
-
+    @ResponseBody
     @PostMapping
     ResponseEntity<GroupReadModel> createGroup(@RequestBody @Valid GroupWriteModel newGroup){
         GroupReadModel outcome = jobGroupService.createGroup(newGroup);
         return ResponseEntity.created(URI.create("/" + outcome.getId())).body(outcome);
     }
-
+    @ResponseBody
     @GetMapping
     ResponseEntity<List<GroupReadModel>> readAllGroups(){
         return ResponseEntity.ok(jobGroupService.readAll().stream().collect(Collectors.toList()));
     }
-
+    @ResponseBody
     @GetMapping("/{id}")
     ResponseEntity<List<Job>> readAllJobsFromGroup(@PathVariable int id){
         return ResponseEntity.ok(jobRepository.findAllByJobGroup_Id(id));
     }
 
-
+    @ResponseBody
     @Transactional
     @PatchMapping("/{id}")
     public ResponseEntity<?> toggleGroup(@PathVariable int id){
