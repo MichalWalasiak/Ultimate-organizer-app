aa27e27c8256f8dc68a5dc4716c71a827ff39f76
Michał Walasiak <m.walasiak@gmail.com>
2020-07-10 13:33:26 +0200

    Add test method

--- src/test/java/io/github/w7mike/model/projection/GroupReadModelTest.java
+++ src/test/java/io/github/w7mike/model/projection/GroupReadModelTest.java
@@ -1,7 +1,14 @@
 package io.github.w7mike.model.projection;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GroupReadModelTest {
+
+    @Test
+    void constructor_noDeadline_createNullDeadline() {
+
+    }
 
 }
