6c7df74aec674d5138d372d93555c0fba4f55d2f
Michał Walasiak <m.walasiak@gmail.com>
2020-07-14 17:17:22 +0200

    Change class name

--- src/main/java/io/github/w7mike/controller/IllegalExceptionsControllerAdvice.java
+++ src/main/java/io/github/w7mike/controller/IllegalExceptionsControllerAdvice.java
@@ -3,5 +3,5 @@ package io.github.w7mike.controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
-public class GlobalControllerAdvice {
+public class IllegalExceptionsControllerAdvice {
 }
