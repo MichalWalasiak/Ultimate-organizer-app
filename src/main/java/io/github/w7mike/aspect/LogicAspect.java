8e8a7c6f4c30ae67ad610f4b3ee56dc628242116
Michał Walasiak <m.walasiak@gmail.com>
2020-07-15 15:32:35 +0200

    Create method with annotation around

--- src/main/java/io/github/w7mike/aspect/LogicAspect.java
+++ src/main/java/io/github/w7mike/aspect/LogicAspect.java
@@ -1,9 +1,16 @@
 package io.github.w7mike.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogicAspect {
+
+    @Around()
+    Object aroundProjectCreateGroup(ProceedingJoinPoint joinPoint) {
+        return joinPoint;
+    }
 }
