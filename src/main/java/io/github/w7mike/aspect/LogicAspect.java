2e3abdc8240e8bf2ff8212122ebc4e5554e8d8da
Michał Walasiak <m.walasiak@gmail.com>
2020-07-15 17:59:08 +0200

    Add method with Before annotation

--- src/main/java/io/github/w7mike/aspect/LogicAspect.java
+++ src/main/java/io/github/w7mike/aspect/LogicAspect.java
@@ -2,9 +2,11 @@ package io.github.w7mike.aspect;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -18,6 +20,11 @@ public class LogicAspect {
 
     public LogicAspect(final MeterRegistry registry) {
         projectCreateGroupTimer = registry.timer("logic.project.create.group");
+    }
+
+    @Before("execution(* io.github.w7mike.logic.ProjectsService.createGroup(..))")
+    void logMethodCall(JoinPoint jp) {
+        logger.info("Before {} with {}", jp.getSignature().getName(), jp.getArgs());
     }
 
     @Around("execution(* io.github.w7mike.logic.ProjectsService.createGroup(..))")
