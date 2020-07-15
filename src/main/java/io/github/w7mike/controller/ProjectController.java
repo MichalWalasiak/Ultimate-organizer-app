ea6ba0d6b69634610c077348ec2a1ad76f560034
Michał Walasiak <m.walasiak@gmail.com>
2020-07-15 14:50:15 +0200

    Add annotation for measurement

--- src/main/java/io/github/w7mike/controller/ProjectController.java
+++ src/main/java/io/github/w7mike/controller/ProjectController.java
@@ -4,6 +4,7 @@ import io.github.w7mike.logic.ProjectsService;
 import io.github.w7mike.model.Project;
 import io.github.w7mike.model.ProjectStep;
 import io.github.w7mike.model.projection.ProjectWriteModel;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -44,6 +45,7 @@ class ProjectController {
         return "projects";
     }
 
+    @Timed(value = "project.create.group", histogram = true, percentiles = {0.5, 0.95, 0.99})
     @PostMapping("/{id}")
     String createGroup(
             @ModelAttribute("project") ProjectWriteModel current,
