a3e8a03f6554bf6c1a345702fb17579a63b01aa2
Michał Walasiak <m.walasiak@gmail.com>
2020-07-20 21:56:29 +0200

    Change access modifier of constructor

--- src/main/java/io/github/w7mike/model/event/JobUndone.java
+++ src/main/java/io/github/w7mike/model/event/JobUndone.java
@@ -5,7 +5,7 @@ import io.github.w7mike.model.Job;
 import java.time.Clock;
 
 public class JobUndone extends JobEvent {
-    public JobUndone(final Job source) {
+    JobUndone(final Job source) {
         super(source.getId(), Clock.systemDefaultZone());
     }
 }
