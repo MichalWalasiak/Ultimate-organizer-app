1e4e41c3ab516f30a2e36a36635805f2f72fffc2
Michał Walasiak <m.walasiak@gmail.com>
2020-07-20 17:39:46 +0200

    Add Logger

--- src/main/java/io/github/w7mike/Warmup.java
+++ src/main/java/io/github/w7mike/Warmup.java
@@ -1,11 +1,15 @@
 package io.github.w7mike;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Warmup implements ApplicationListener<ContextRefreshedEvent> {
+    private static final Logger logger = LoggerFactory.getLogger(Warmup.class);
+
     @Override
     public void onApplicationEvent(final ContextRefreshedEvent event) {
 
