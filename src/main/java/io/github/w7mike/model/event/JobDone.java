abd2c64758d7cb7d48b76bb0c1167c811f3d48e1
Michał Walasiak <m.walasiak@gmail.com>
2020-07-20 21:54:06 +0200

    Create new class with constructor

---
+++ src/main/java/io/github/w7mike/model/event/JobDone.java
@@ -0,0 +1,12 @@
+package io.github.w7mike.model.event;
+
+import io.github.w7mike.model.Job;
+
+import java.time.Clock;
+
+public class JobDone extends JobEvent {
+
+    public JobDone(final Job source) {
+        super(source.getId(), Clock.systemDefaultZone());
+    }
+}
