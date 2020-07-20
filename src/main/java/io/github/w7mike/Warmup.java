a51ee151ec83a71ad6d27a78f1fbb344404a0c76
Michał Walasiak <m.walasiak@gmail.com>
2020-07-20 17:41:41 +0200

    Add private filed and add it to constructor

--- src/main/java/io/github/w7mike/Warmup.java
+++ src/main/java/io/github/w7mike/Warmup.java
@@ -1,5 +1,6 @@
 package io.github.w7mike;
 
+import io.github.w7mike.model.JobGroupRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
@@ -9,6 +10,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class Warmup implements ApplicationListener<ContextRefreshedEvent> {
     private static final Logger logger = LoggerFactory.getLogger(Warmup.class);
+
+    private final JobGroupRepository groupRepository;
+
+    public Warmup(final JobGroupRepository groupRepository) {
+        this.groupRepository = groupRepository;
+    }
 
     @Override
     public void onApplicationEvent(final ContextRefreshedEvent event) {
