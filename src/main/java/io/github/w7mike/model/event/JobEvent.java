03d88f22e7707e1264e45bf0e7316e22eaece576
Michał Walasiak <m.walasiak@gmail.com>
2020-07-20 19:07:34 +0200

    Add constructor

--- src/main/java/io/github/w7mike/model/event/JobEvent.java
+++ src/main/java/io/github/w7mike/model/event/JobEvent.java
@@ -1,10 +1,16 @@
 package io.github.w7mike.model.event;
 
+import java.time.Clock;
 import java.time.Instant;
 
 public abstract class JobEvent {
     private Integer jobId;
     private Instant occurrence;
+
+    public JobEvent(final Integer jobId, Clock clock) {
+        this.jobId = jobId;
+        this.occurrence = Instant.now(clock);
+    }
 
     public Integer getJobId() {
         return jobId;
