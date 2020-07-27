97220cd850595bd3f91539aad40afae09eb8e021
Michał Walasiak <m.walasiak@gmail.com>
2020-07-27 13:05:08 +0200

    Add EventListener methods to class

--- src/main/java/io/github/w7mike/reports/ChangedJobEventListener.java
+++ src/main/java/io/github/w7mike/reports/ChangedJobEventListener.java
@@ -1,11 +1,24 @@
 package io.github.w7mike.reports;
 
+import io.github.w7mike.model.event.JobDone;
+import io.github.w7mike.model.event.JobUndone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChangedJobEventListener {
 
     private final Logger logger = LoggerFactory.getLogger(ChangedJobEventListener.class);
+
+    @EventListener
+    public void on(JobDone event) {
+        logger.info("Got " + event);
+    }
+
+    @EventListener
+    public void on(JobUndone event) {
+        logger.info("Got " + event);
+    }
 }
