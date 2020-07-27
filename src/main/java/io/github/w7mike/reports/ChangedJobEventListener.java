08223ec310b59580822a7006a492838829d8c26a
Michał Walasiak <m.walasiak@gmail.com>
2020-07-27 12:18:00 +0200

    Add Logger to class

--- src/main/java/io/github/w7mike/reports/ChangedJobEventListener.java
+++ src/main/java/io/github/w7mike/reports/ChangedJobEventListener.java
@@ -1,7 +1,11 @@
 package io.github.w7mike.reports;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChangedJobEventListener {
+
+    private final Logger logger = LoggerFactory.getLogger(ChangedJobEventListener.class);
 }
