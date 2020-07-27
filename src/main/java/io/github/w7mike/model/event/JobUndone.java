b95375ae815afa11153e3cbb14e47fd55de6f2b7
Michał Walasiak <m.walasiak@gmail.com>
2020-07-27 13:06:46 +0200

    Change parameter in constructor to BaseJob

--- src/main/java/io/github/w7mike/model/event/JobUndone.java
+++ src/main/java/io/github/w7mike/model/event/JobUndone.java
@@ -1,11 +1,12 @@
 package io.github.w7mike.model.event;
 
+import io.github.w7mike.model.BaseJob;
 import io.github.w7mike.model.Job;
 
 import java.time.Clock;
 
 public class JobUndone extends JobEvent {
-    JobUndone(final Job source) {
+    JobUndone(final BaseJob source) {
         super(source.getId(), Clock.systemDefaultZone());
     }
 }
