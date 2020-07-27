596199bc4a1195a033b1affdf1a3bf9696d33db5
Michał Walasiak <m.walasiak@gmail.com>
2020-07-27 13:05:35 +0200

    Change parameter to BaseJob

--- src/main/java/io/github/w7mike/model/event/JobDone.java
+++ src/main/java/io/github/w7mike/model/event/JobDone.java
@@ -1,12 +1,13 @@
 package io.github.w7mike.model.event;
 
+import io.github.w7mike.model.BaseJob;
 import io.github.w7mike.model.Job;
 
 import java.time.Clock;
 
 public class JobDone extends JobEvent {
 
-    JobDone(final Job source) {
+    JobDone(final BaseJob source) {
         super(source.getId(), Clock.systemDefaultZone());
     }
 }
