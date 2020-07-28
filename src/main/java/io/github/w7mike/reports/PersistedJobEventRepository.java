d0c27576c3e9481407037197e1fa832ae24f89f1
Michał Walasiak <m.walasiak@gmail.com>
2020-07-28 12:16:19 +0200

    Add method to repository

--- src/main/java/io/github/w7mike/reports/PersistedJobEventRepository.java
+++ src/main/java/io/github/w7mike/reports/PersistedJobEventRepository.java
@@ -2,5 +2,8 @@ package io.github.w7mike.reports;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersistedJobEventRepository extends JpaRepository<PersistedJobEvent, Integer> {
+    List<PersistedJobEvent> findByJobId(Integer jobId);
 }
