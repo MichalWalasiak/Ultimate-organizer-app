a76882f9b78266512369c4d7fa08aa646512a1b7
Michał Walasiak <m.walasiak@gmail.com>
2020-07-28 11:26:05 +0200

    Updates in toggleJob method

--- src/main/java/io/github/w7mike/controller/JobController.java
+++ src/main/java/io/github/w7mike/controller/JobController.java
@@ -75,7 +75,7 @@ class JobController {
 
     @Transactional
     @PatchMapping("/{id}")
-    public ResponseEntity<Job> toggleJob(@PathVariable int id){
+    public ResponseEntity<?> toggleJob(@PathVariable int id){
         if(!repository.existsById(id)){
             return ResponseEntity.notFound().build();
         }
@@ -83,12 +83,7 @@ class JobController {
         repository.findById(id)
                 .map(Job::toggle)
                 .ifPresent(eventPublisher::publishEvent);
-        return ResponseEntity.notFound().build();
-
-        /*Job job = repository.findById(id).get();
-        job.setComplete(!job.isComplete());
-
-        return ResponseEntity.ok(job);*/
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
