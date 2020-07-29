c323fdeba733d6c43d1b7366bb38de9b06ff42d8
Michał Walasiak <m.walasiak@gmail.com>
2020-07-29 14:47:21 +0200

    Add annotation to class

--- src/main/java/io/github/w7mike/reports/ReportController.java
+++ src/main/java/io/github/w7mike/reports/ReportController.java
@@ -6,10 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@Controller("/reports")
+@Controller
+@RequestMapping("/reports")
 public class ReportController {
     private final JobRepository repository;
     private final PersistedJobEventRepository persistedJobEventRepository;
