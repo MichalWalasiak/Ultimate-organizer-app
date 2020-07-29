0353d55a401c9734527e1a3a69842a6b74b00ed6
Michał Walasiak <m.walasiak@gmail.com>
2020-07-29 14:58:19 +0200

    Add flag EnableAsync

--- src/main/java/io/github/w7mike/UltimateOrganizerAppApplication.java
+++ src/main/java/io/github/w7mike/UltimateOrganizerAppApplication.java
@@ -3,11 +3,12 @@ package io.github.w7mike;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.validation.Validator;
 
-
+@EnableAsync
 @SpringBootApplication
 public class UltimateOrganizerAppApplication {
 
