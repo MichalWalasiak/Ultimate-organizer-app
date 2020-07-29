084d27703a3697422d80bf9d0215f14e0c650700
Michał Walasiak <m.walasiak@gmail.com>
2020-07-29 14:18:34 +0200

    Initialize private fields in constructor

--- src/main/java/io/github/w7mike/reports/ReportController.java
+++ src/main/java/io/github/w7mike/reports/ReportController.java
@@ -1,7 +1,15 @@
 package io.github.w7mike.reports;
 
+import io.github.w7mike.model.JobRepository;
 import org.springframework.stereotype.Controller;
 
 @Controller("/reports")
 public class ReportController {
+    private final JobRepository repository;
+    private final PersistedJobEventRepository persistedJobEventRepository;
+
+    public ReportController(final JobRepository repository, final PersistedJobEventRepository persistedJobEventRepository) {
+        this.repository = repository;
+        this.persistedJobEventRepository = persistedJobEventRepository;
+    }
 }
