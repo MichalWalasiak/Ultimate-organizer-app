88902ed5fc348593456e94d912a3806a29499dd3
Michał Walasiak <m.walasiak@gmail.com>
2020-07-29 14:32:09 +0200

    Add method to read job with changes

--- src/main/java/io/github/w7mike/reports/ReportController.java
+++ src/main/java/io/github/w7mike/reports/ReportController.java
@@ -1,7 +1,10 @@
 package io.github.w7mike.reports;
 
 import io.github.w7mike.model.JobRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller("/reports")
 public class ReportController {
@@ -11,5 +14,13 @@ public class ReportController {
     public ReportController(final JobRepository repository, final PersistedJobEventRepository persistedJobEventRepository) {
         this.repository = repository;
         this.persistedJobEventRepository = persistedJobEventRepository;
+    }
+
+    @GetMapping("/count/{id}")
+    ResponseEntity<JobWithChangesCount> readJobWithCount(@PathVariable Integer id) {
+        return repository.findById(id)
+                .map(job -> new JobWithChangesCount(job, persistedJobEventRepository.findByJobId(id)))
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
